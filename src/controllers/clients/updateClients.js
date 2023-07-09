2c58bffc27ca50c2d7d64262246178d42e34268d
HopeAero <davidsalcedo388@gmail.com>
2023-07-09 12:10:27 -0400

    update client sin validacion

--- src/controllers/clients/updateClients.js
+++ src/controllers/clients/updateClients.js
@@ -5,52 +5,64 @@ const updateClient = async (req, res) => {
             const ci_cliente = req.params.ci;
             const telefonoPrincipal = req.body.telefono_principal;
             const telefonoSecundario = req.body.telefono_secundario;
-            const verifyPhone = await pool.query('SELECT * FROM cliente WHERE telefono_principal = $1', [telefonoPrincipal]);
-            const verify = verifyPhone.rows.find(element => {
-                return element;
-            });
 
-            if (verifyPhone.rows.length !== 0 && verify.ci_cliente !== ci_cliente) {
+            if (telefonoPrincipal === '' || telefonoPrincipal === undefined) {
                 res.status(404).json({
-                    success: false,
-                    message: "Ya existe cliente con este telefono",
+                    success: true,
+                    message: "Telefono principal ingresado no puede ser nulo",
+                    items: response.rows
                 });
-
             } else {
-                if (telefonoSecundario !== undefined && telefonoSecundario !== null) {
-                    const verifyPhone2 = await pool.query('SELECT * FROM cliente WHERE telefono_secundario = $1', [telefonoSecundario]);
-                    const verify2 = verifyPhone2.rows.find(element => {
-                        return element;
+                if (telefonoSecundario !== null && telefonoSecundario.length > 11){
+                    res.status(404).json({
+                        success: false,
+                        message: "Telefono secundario debe tener maximo 11 digitos",
                     });
-
-                    if (verifyPhone2.rows.length !== 0 && verify2.ci_cliente !== ci_cliente) {
-                        res.status(404).json({
-                            success: false,
-                            message: "Ya existe cliente con este telefono",
-                        });
-                    } else {
-                        const {nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente} = req.body;
+                } else {
+                    if (telefonoPrincipal === null && telefonoSecundario === null) {
+                        const {nombre, direccion, correo_electronico, es_frecuente} = req.body;
                         const response = await pool.query(
-                        'UPDATE cliente SET nombre = $1, direccion = $2, telefono_principal = $3, telefono_secundario = $4, correo_electronico = $5, es_frecuente = $6 WHERE ci_cliente = $7 RETURNING *', [nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente, ci_cliente]);
-    
+                            'UPDATE cliente SET nombre = $1, direccion = $2, correo_electronico = $3, es_frecuente = $4 WHERE ci_cliente = $5 RETURNING *', [nombre, direccion,correo_electronico, es_frecuente, ci_cliente]);
+            
                         res.status(200).json({
                             success: true,
                             message: "Cliente actualizado con exito",
                             items: response.rows
-                        });  
+                        });      
+                    } else {
+                        if (telefonoSecundario !== null && telefonoSecundario !== undefined && telefonoSecundario !== '') {
+                            if (telefonoPrincipal === telefonoSecundario) {
+                            res.status(404).json({
+                                success: false,
+                                message: "El numero principal no puede ser igual al secundario",
+                                items: response.rows
+                            });
+                            } else {
+                                const {nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente} = req.body;
+                                const response = await pool.query(
+                                'UPDATE cliente SET nombre = $1, direccion = $2, telefono_principal = $3, telefono_secundario = $4, correo_electronico = $5, es_frecuente = $6 WHERE ci_cliente = $7 RETURNING *', [nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente, ci_cliente]);
+                
+                                res.status(200).json({
+                                    success: true,
+                                    message: "Cliente actualizado con exito",
+                                    items: response.rows
+                                });  
+                            }  
+                        } else {
+                            const {nombre, direccion, telefono_principal, correo_electronico, es_frecuente} = req.body;
+                            const response = await pool.query(
+                                'UPDATE cliente SET nombre = $1, direccion = $2, telefono_principal = $3, correo_electronico = $4, es_frecuente = $5 WHERE ci_cliente = $6 RETURNING *', [nombre, direccion, telefono_principal ,correo_electronico, es_frecuente, ci_cliente]);
+                                res.status(200).json({
+                                    success: true,
+                                    message: "Cliente actualizado con exito",
+                                    items: response.rows
+                                });
+                            }
                     }
-                } else {
-                    const {nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente} = req.body;
-                    const response = await pool.query(
-                    'UPDATE cliente SET nombre = $1, direccion = $2, telefono_principal = $3, telefono_secundario = $4, correo_electronico = $5, es_frecuente = $6 WHERE ci_cliente = $7 RETURNING *', [nombre, direccion, telefono_principal, telefono_secundario, correo_electronico, es_frecuente, ci_cliente]);
-    
-                    res.status(200).json({
-                        success: true,
-                        message: "Cliente actualizado con exito",
-                        items: response.rows
-                    });  
-                }
-            }
+                    
+                } 
+            }                
+                
 
     } catch(error) {
         res.status(500).json({

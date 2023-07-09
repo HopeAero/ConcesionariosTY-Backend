02645901962673e47bf764dbf492ce29b2294fb7
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-09 17:40:26 -0400

    Endpoints de Linea de Suministro
    Bellaco

--- src/controllers/supply-line/deleteLine.js
+++ src/controllers/supply-line/deleteLine.js
@@ -1,25 +1,22 @@
 const {pool} = require('../../databases/db');
 
-const posterLine = async (req, res) => {
+const deleteLine = async (req, res) => {
     try {
-        const codigo = req.body.codigo;
+        const codigo = req.params.codigo;
         const verify = await pool.query('SELECT * FROM linea_suministro WHERE codigo = $1', [codigo]);
 
-        if (verify.rows.length !== 0) {
+        if (verify.rows.length === 0) {
             res.status(404).json({
                 success: false,
-                message: "Ya existe linea de suministro con este codigo",
+                message: "No existe esta linea de suministro con este codigo",
             });
         } else {
-            const {descripcion} = req.body;
-            const response = await pool.query('INSERT INTO linea_suministro (codigo, descripcion) VALUES ($1, $2) RETURNING *', [codigo, descripcion]);
+            await pool.query('DELETE FROM linea_suministro WHERE codigo = $1', [codigo]);
             res.status(200).json({
                 success: true,
-                message: "Linea de suministro insertada con exito",
-                items: response.rows
+                message: "Linea de suministro eliminada con exito",
             });
         }
-
     } catch (error) {
         res.status(500).json({
             success: false,
@@ -29,8 +26,6 @@ const posterLine = async (req, res) => {
     }
 }
 
-
 module.exports = {
-    posterLine
+    deleteLine
 }
-

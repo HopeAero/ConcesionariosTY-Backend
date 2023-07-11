2dda374c3f0fb21581c137e16cb4e753fc966d84
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-10 20:58:26 -0400

    Creacion del GetAll

--- src/controllers/State/getState.js
+++ src/controllers/State/getState.js
@@ -53,7 +53,27 @@ const getStateById = async (req, res) => {
     }
 }
 
+const getAllState = async (req, res) => {
+    try {
+        const response = await pool.query('SELECT * FROM estado');
+
+        res.status(200).json({
+            success: true,
+            message: "Estados recuperados con exito",
+            items: response.rows
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            success: false,
+            message: "Ha ocurrido un problema",
+        });
+        console.log(error);
+    }
+}  
+
 module.exports = {
     getState,
-    getStateById
+    getStateById,
+    getAllState
 }

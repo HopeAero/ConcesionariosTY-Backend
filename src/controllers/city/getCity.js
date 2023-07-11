144a9cd107af4386aa0be7fa2aff5c120e638062
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-10 21:00:06 -0400

    Creacion del GetAll

--- src/controllers/city/getCity.js
+++ src/controllers/city/getCity.js
@@ -62,7 +62,26 @@ const getCityByState = async (req, res) => {
     }
 }
 
+const getAllCity = async (req, res) => {
+    try {
+        const response = await pool.query('SELECT * FROM ciudad ORDER BY id_estado, nro_ciudad');
+
+        res.status(200).json({
+            success: true,
+            message: "Ciudades recuperadas con éxito",
+            items: response.rows
+        });
+    } catch(error) {
+        res.status(500).json({
+            success: false,
+            message: "Ha ocurrido un problema",
+        });
+        console.log(error);
+    }
+}
+
 module.exports = {
     getCity,
-    getCityByState
+    getCityByState,
+    getAllCity
 }

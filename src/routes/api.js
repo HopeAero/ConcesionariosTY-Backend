075cc3b514a61774adb97e42b285fcc329a0fdd1
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-11 01:23:46 -0400

    Endpoints Analistas

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,9 +3,10 @@ const router = Router();
 
 const apiClientsRouter = require('./api/clientes');
 const apiEmployeesRouter = require('./api/empleados');
+const apiAnalystsRouter = require('./api/analistas');
 
 router.use('/clientes', apiClientsRouter);
 router.use('/empleados', apiEmployeesRouter);
-
+router.use('/analistas', apiAnalystsRouter);
 
 module.exports = router;

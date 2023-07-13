4d29981edf13a111515c14f31b25c141229fb755
Edusus <ejsucre.19@est.ucab.edu.ve>
2023-07-13 18:42:45 -0400

    endpoint getAllClients

--- src/routes/api/clientes.js
+++ src/routes/api/clientes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {getClients, getClientByCI} = require('./../../controllers/clients/getClients');
+const {getClients, getClientByCI, getAllClients} = require('./../../controllers/clients/getClients');
 const {posterClients} = require('./../../controllers/clients/posterClients');
 const {updateClient} = require('./../../controllers/clients/updateClients');
 const {deleteClient} = require('../../controllers/clients/deleteClients');
 
 router.get('/', getClients);
+
+router.get('/all', getAllClients);
 
 router.get('/:ci', getClientByCI);
 

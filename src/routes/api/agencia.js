b7cfa8510949b40f5b220aa5fed38f04b2405893
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-13 13:13:49 -0400

    Get AgenciaALL

--- src/routes/api/agencia.js
+++ src/routes/api/agencia.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const {getAgency, getAgencyByRIF} = require('./../../controllers/agency/getAgency');
+const {getAgency, getAgencyByRIF, getAgencyAll} = require('./../../controllers/agency/getAgency');
 const {posterAgency} = require('./../../controllers/agency/posterAgency');
 const {updateAgency} = require('./../../controllers/agency/updateAgency');
 const {deleteAgency} = require('./../../controllers/agency/deleteAgency');
 
 router.get('/', getAgency);
+
+router.get('/all', getAgencyAll);
 
 router.get('/:rif', getAgencyByRIF);
 

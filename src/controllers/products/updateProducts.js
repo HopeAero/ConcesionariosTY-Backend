6640c25554f7c5a9deaa5d386e1ce17a1f08db69
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-07-13 22:30:11 -0400

    Head Backend Moment

--- src/controllers/products/updateProducts.js
+++ src/controllers/products/updateProducts.js
@@ -2,11 +2,11 @@ const { pool } = require('../../databases/db');
 
 const updateProduct = async (req, res) => {
     try {
-        const codigo = req.params.codigo;
+        const codigoProducto = req.params.codigo;
         const codigo_linea_s = req.params.codigo_linea_s;
-        const { codigo_linea_sNew, nombre_corto } = req.body;
+        const { codigo_linea_sNew, nombre_corto, descripcion, es_ecologico, precio, existencia, existencia_minima, existencia_maxima, proveedor } = req.body;
 
-        const verify = await pool.query('SELECT * FROM linea_suministro WHERE codigo = $1', [codigo_linea_s]);
+        const verify = await pool.query('SELECT * FROM producto WHERE codigo = $1', [codigo_linea_s]);
         const verify2 = await pool.query('SELECT * FROM linea_suministro WHERE codigo = $1', [codigo_linea_sNew]);
 
         console.log(codigo);
@@ -20,7 +20,7 @@ const updateProduct = async (req, res) => {
             });
 
         } else {
-            const response = await pool.query('UPDATE producto SET codigo_linea_s = $1, nombre_corto = $2, descripcion = $3, es_ecologico = $4, precio = $5, existencia = $6, existencia_minima = $7, existencia_maxima = $8, proveedor = $9 WHERE codigo = $10 AND codigo_linea_s = $11 RETURNING *', [codigo_linea_sNew, nombre_corto, descripcion, es_ecologico, precio, existencia, existencia_minima, existencia_maxima, proveedor, codigo, codigo_linea_s]);
+            const response = await pool.query('UPDATE producto SET codigo_linea_s = $1, nombre_corto = $2, descripcion = $3, es_ecologico = $4, precio = $5, existencia = $6, existencia_minima = $7, existencia_maxima = $8, proveedor = $9 WHERE codigo = $10 AND codigo_linea_s = $11 RETURNING *', [codigo_linea_sNew, nombre_corto, descripcion, es_ecologico, precio, existencia, existencia_minima, existencia_maxima, proveedor, codigoProducto, codigo_linea_s]);
 
             res.status(200).json({
                 success: true,

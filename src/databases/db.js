1f3b6e2d2983cacc73f41a0d0340aa14272971eb
Paniagua <114714690+luizzzito@users.noreply.github.com>
2023-07-14 08:00:22 -0400

    Update db.js

--- src/databases/db.js
+++ src/databases/db.js
@@ -3,7 +3,7 @@ const {Pool} = require('pg');
 const pool = new Pool({
     host: 'localhost',  
     user: 'postgres',
-    password: 'anna2018',
+    password: '2001',
     database: 'ConcesionariosTY',
     port: '5432'
 });

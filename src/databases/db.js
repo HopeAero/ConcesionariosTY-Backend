20050822a66cfcca5b1a8d475712ce7e7c0207f5
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-08-08 23:14:07 -0400

    intentossl

--- src/databases/db.js
+++ src/databases/db.js
@@ -1,6 +1,5 @@
 const {Pool} = require('pg');
 const fs = require('fs');
-console.log(process.env.PGSSLMODE);
 
 const pool = new Pool({
     host: process.env.HOST || 'localhost',  
@@ -8,12 +7,10 @@ const pool = new Pool({
     password: process.env.PASSWORD || '1234',
     database: process.env.DATABASE || 'ConcesionariosTY',
     port: process.env.PORT || 5432,
-    database_url: process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/ConcesionariosTY',
+    connectionString: process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/ConcesionariosTY',
     ssl : {
-        rejectUnauthorized: true,
-        ca: fs.readFileSync(
-            `${process.cwd()}/cert/ca-certificate.crt`.toString()
-            ),
+        require: true,
+        rejectUnauthorized: false
     }
 });
 

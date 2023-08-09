3ee7a768e486d9e0056f0e2d49529d502ef37eee
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2023-08-08 22:36:26 -0400

    Update db.js

--- src/databases/db.js
+++ src/databases/db.js
@@ -4,8 +4,9 @@ const pool = new Pool({
     host: process.env.HOST || 'localhost',  
     user: process.env.USER || 'postgres',
     password: process.env.PASSWORD || '1234',
-    database: process.env.DATABASE || 'prueba',
-    port: process.env.PORT || 5432
+    database: process.env.DATABASE || 'ConcesionariosTY',
+    port: process.env.PORT || 5432,
+    database_url: process.env.DATABASE_URL || 'postgres://postgres:1234@localhost:5432/ConcesionariosTY'
 });
 
 module.exports = {
